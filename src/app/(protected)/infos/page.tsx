803393f8e5aeb6ada884bb5f5ddd34abd6f9be56
admin BERTHO Mikael <adm-mibertho@intra.chu-nantes.fr>
2025-07-02 13:32:25 +0200

    correction deployement

--- src/app/(protected)/infos/page.tsx
+++ src/app/(protected)/infos/page.tsx
@@ -2,7 +2,7 @@ export default function InfoPage() {
   return (
     <div className="max-w-3xl mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Infos pratiques</h1>
-      <p>Informations utiles sur le mariage, le lieu, l'hébergement, etc.</p>
+      <p>Informations utiles sur le mariage, le lieu, l&#39;hébergement, etc.</p>
     </div>
   )
 }
