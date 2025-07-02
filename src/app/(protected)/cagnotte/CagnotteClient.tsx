a3c94283d7e733887602404b27e75227a9498f10
admin BERTHO Mikael <adm-mibertho@intra.chu-nantes.fr>
2025-07-02 14:09:19 +0200

    ajout page cagnotte et sondage

--- src/app/(protected)/cagnotte/CagnotteClient.tsx
+++ src/app/(protected)/cagnotte/CagnotteClient.tsx
@@ -1,13 +1,15 @@
 'use client'
 
-
 export default function CagnotteClient() {
+
 
   return (
     <div>
+          <div>
       <div className="max-w-3xl mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Ici sera peut être la cagnotte</h1>
      
+    </div>
     </div>
     </div>
   )
