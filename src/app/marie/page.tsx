f8aa7ffa4719fa32d680bc84f16274436ca43fa7
Guillaume Ferrand <ferrand.guillaume8@gmail.com>
2025-08-13 18:31:50 +0200

    correction

--- src/app/marie/page.tsx
+++ src/app/marie/page.tsx
@@ -1,16 +1,13 @@
 import { redirect } from 'next/navigation'
 
-interface PageProps {
-  searchParams?: { [key: string]: string | undefined }
-}
-
-export default function Page({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
+export default function Page({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) {
   const token = searchParams?.token
   const SECRET_TOKEN = 'ton_token_secret'
 
   if (token !== SECRET_TOKEN) {
-    redirect('/') // redirection côté serveur
+    redirect('/')
+    return
   }
 
-  redirect(`/marie/view?token=${token}`) // redirection côté serveur
+  redirect(`/marie/view?token=${token}`)
 }
