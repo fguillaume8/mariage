7fed8a4decdb256d7347859c445aed8e4f922d16
Guillaume Ferrand <ferrand.guillaume8@gmail.com>
2025-08-13 18:35:04 +0200

    correction

--- src/app/marie/page.tsx
+++ src/app/marie/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
-export default function Page({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) {
+export default function Page({ searchParams }: any) {
   const token = searchParams?.token
   const SECRET_TOKEN = 'ton_token_secret'
 
