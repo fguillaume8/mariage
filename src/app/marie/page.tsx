f2c701e650bfff3670ea245097ad12ccea9385c1
Guillaume Ferrand <ferrand.guillaume8@gmail.com>
2025-08-13 17:57:42 +0200

    correction marie

--- src/app/marie/page.tsx
+++ src/app/marie/page.tsx
@@ -1,15 +1,12 @@
 import { redirect } from 'next/navigation'
 
-export default async function Page({ searchParams }: { searchParams: Promise<{ [key: string]: string | undefined }> }) {
-  const params = await searchParams
-  const token = params.token
-
-  const SECRET_TOKEN = 'ton_token_secret' // ← à personnaliser
+export default function Page({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
+  const token = searchParams.token
+  const SECRET_TOKEN = 'ton_token_secret' // à personnaliser
 
   if (token !== SECRET_TOKEN) {
     redirect('/')
   }
 
-  // Redirige vers la vraie page client protégée avec le token
   redirect(`/marie/view?token=${token}`)
 }
