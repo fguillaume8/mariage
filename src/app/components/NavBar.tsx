3add1aec28f492bb874f2acd9e5e60fcfe58247c
Ferrand <ferrand.guillaume8@gmail.com>
2025-08-27 11:20:10 +0200

    ajout image png

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -62,7 +62,7 @@ export default function NavBar() {
 
 <div className="flex justify-end items-center relative overflow-visible h-7">
   <img
-    src="image/logo-200.png"
+    src="image/Logo_200.png"
     alt="Logo mariage"
     className="h-[140%] object-contain "
   />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -163,7 +163,7 @@ export default function Home() {
         {/* Droite */}
         <div className="flex justify-end items-center h-full">
           <img
-            src="image/logo-200.png"
+            src="image/Logo_200.png"
             alt="Logo mariage"
             className="h-150 w-auto object-contain"
           />
